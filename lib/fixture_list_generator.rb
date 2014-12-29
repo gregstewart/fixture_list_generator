5336d99c00d93fc69540bd81e70fae8fb57d220b
Greg Stewart <gregs@tcias.co.uk>
2014-12-29 11:16:59 +0000

    returns the right number of fixtures for a given team list, however not sorted properly

--- lib/fixture_list_generator.rb
+++ lib/fixture_list_generator.rb
@@ -2,12 +2,49 @@ require "fixture_list_generator/version"
 
 # All code in the gem is namespaced under this module.
 module FixtureListGenerator
-  def FixtureListGenerator.generate list
-    number_of_fixtures  = (list.length-1)*2
-    fixture_list = Array.new(number_of_fixtures) { Hash.new }
+  def FixtureListGenerator.generate array_of_teams
+    number_of_fixtures  = (array_of_teams.length-1)*2
+    fixtures = Array.new(number_of_fixtures) { {"week" => nil, "matches" => []} }
+    duplicate_array_of_teams = array_of_teams.dup
+    matches = []
+
+    fixtures.map.with_index do |week, i|
+      week["week"] = i+1
+    end
 
 
+    while !duplicate_array_of_teams.empty? do
+      current_team = duplicate_array_of_teams.pop
 
-    fixture_list
+      array_of_teams.each do |opponent|
+        if opponent != current_team
+
+          match_hash = {"home" => current_team, "away" => opponent}
+          matches.push match_hash
+        end
+      end
+    end
+
+    current_team
+    start_index = 0
+    end_index = number_of_fixtures-1
+    while !matches.empty? do
+      home_leg = matches.shift()
+      away_leg = matches.pop()
+
+      if current_team != home_leg["home"]
+        current_team = home_leg["home"]
+        start_index = 0
+        end_index = number_of_fixtures-1
+      end
+
+      fixtures[start_index]["matches"].push(home_leg)
+      fixtures[end_index]["matches"].push(away_leg)
+
+      start_index += 1
+      end_index -= 1
+    end
+
+    fixtures
   end
 end
--- test/fixture_list_test.rb
+++ test/fixture_list_test.rb
@@ -4,16 +4,28 @@ require "fixture_list_generator"
 
 describe "Fixture List Generator" do
   before do
-    @list = [1,2,3,4,5,6,7,8,9,10]
-    @expected_number_of_fixtures = (@list.length-1)*2
+    @list = [1,2,3,4]
+    @expected_number_of_fixture_weeks = (@list.length-1)*2
+    @expected_number_of_games_per_week = @list.length/2
   end
 
   it "must not be empty" do
-    FixtureListGenerator::generate(@list).wont_be_nil
+    skip
+    generated_fixture_list = FixtureListGenerator::generate(@list)
+    generated_fixture_list.wont_be_nil
   end
 
-  it "returns an array with a length of #{@expected_number_of_fixtures}" do
-    result = FixtureListGenerator::generate(@list)
-    result.count.must_equal @expected_number_of_fixtures
+  it "returns an array with a length of #{@expected_number_of_fixture_weeks}" do
+    skip
+    generated_fixture_list = FixtureListGenerator::generate(@list)
+    generated_fixture_list.count.must_equal @expected_number_of_fixture_weeks
+  end
+
+  describe "each week" do
+    it "has a #{@expected_number_of_games_per_week}" do
+      generated_fixture_list = FixtureListGenerator::generate(@list)
+      puts generated_fixture_list
+      generated_fixture_list[0]['matches'].length.must_equal @expected_number_of_games_per_week
+    end
   end
 end
